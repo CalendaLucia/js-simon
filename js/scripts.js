1fa75045bf900c20430b348f1fe809177f88f8d0
CalendaLucia <calendalucia4@gmail.com>
2023-01-15 17:47:23 +0100

    altra versione

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 const play = document.getElementById('button');
 let randomNumbers = [];
 
-play.addEventListener('click', function () {
+const text = document.querySelector('.time-out');
+const screen = document.getElementById('container-number');
+const seconds = document.querySelector('.seconds');
 
-    const text = document.querySelector('.time-out');
-    const screen = document.getElementById('container-number');
-    text.style.display = 'none';
+function startGame() {
 
-
+    // Reinizializza l'array prima di generare nuovi numeri casuali
+    randomNumbers = [];
+    // Clear the screen before creating new elements
+    while (screen.firstChild) {
+        screen.removeChild(screen.firstChild);
+    }
     // Generate 5 random numbers
     for (let i = 0; i < 5; i++) {
 
@@ -16,75 +21,64 @@ play.addEventListener('click', function () {
         const number = document.createElement('span');
         number.classList.add('number');
         number.innerHTML = randomNumber;
+        number.style.display = 'block';
         screen.append(number);
     }
 
-    const seconds = document.querySelector('.seconds');
+    text.style.display = 'none';
     let counter = 30;
     const timer = setInterval(function () {
         seconds.innerText = counter;
 
-
         if ( counter == 0) {
 
-                
-          clearInterval(timer);
-          screen.style.display = 'none';
-          text.style.display = 'block';
-          text.innerHTML = 'Tempo Scaduto';
-          seconds.innerText = '00' ;
-          setTimeout(response, 3000);
-         
-         } 
+            clearInterval(timer);
+            text.style.display = 'block';
+            text.innerHTML = 'Tempo Scaduto';
+            seconds.innerText = '00' ;
+            setTimeout(response, 3000);
+        } 
 
-         if (counter <= 9) {
+        if (counter <= 9) {
 
-           seconds.innerText = '0' + counter-- ; 
+            seconds.innerText = '0' + counter-- ; 
 
-         } else {
+        } else {
 
-          console.log(counter);
-          counter--;
-
-         }
-      
-     }, 1000);
-
-    function response() {
-      
-        let correctNumbers = 0;
-        let correctNumbersList = [];
-
-        for (let i = 0; i < 5; i++) {
-
-            const remember = parseInt(prompt('Scrivi in sequenza i numeri che hai visto'));
-
-            if (remember === randomNumbers[i]) {
-
-                correctNumbers++;
-                correctNumbersList.push(remember);
-            }
+           counter--;
         }
+    }, 1000);
 
-        text.innerHTML = 'Hai indovinato ' + correctNumbers + ' numeri: ' + correctNumbersList;
-   
-      
-      
+}
+
+function response() {
+    
+    let correctNumbers = 0;
+    let correctNumbersList = [];
+
+    for (let i = 0; i < 5; i++) {
+
+        const remember = parseInt(prompt('Scrivi in sequenza i numeri che hai visto'));
+
+        if (remember === randomNumbers[i]) {
+            correctNumbers++;
+            correctNumbersList.push(remember);
+        }
+    }
+    text.innerHTML = 'Hai indovinato ' + correctNumbers + ' numeri: ' + correctNumbersList;
+    play.innerHTML = 'Rigioca';
+    play.removeEventListener('click', startGame);
+    play.addEventListener('click', restartGame);
+}
+
+
+
+
+    function restartGame() {
+        play.innerHTML = 'Gioca';
+        play.removeEventListener('click', restartGame);
+        play.addEventListener('click', startGame);
+        startGame();
     }
 
-
-});
-
-restart();
-
-// function restart() {
-
-//     randomNumbers = [];
-//     text.innerHTML = 'Hai 30 secondi';
-//     text.style.display = 'block';
-//     screen.innerHTML = '';
-//     screen.style.display = 'block';
-//     seconds.innerText = '30';
-
-//     } da rivedere
-    
+    play.addEventListener('click', startGame);
